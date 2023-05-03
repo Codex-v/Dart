972b09e7bfce84736dd6a8f832fdb95fff57f530
VEDS03 <veds2103@gmail.com>
2023-05-04 00:45:15 +0530

    var keyword in dart

---
+++ var_keyword.dart
@@ -0,0 +1,25 @@
+void main() {
+  var a = {'ved': 4, 78.5: "shah"}; //map
+  var b = [8, 9, 7, 8]; //List
+  var s = "ved"; //string
+  var k = 78; // int
+  var duble = 8.5; //double
+  var bo = true; //bool
+  print(bo.runtimeType);
+
+  /**
+  var keyword in dart programming
+
+  var is a keyword, that means "I don't care to notate what the type is here".
+  var a = "ved";
+
+  simply dart will replace the var keyword with initializer type or leave it dynamic by default if there is no initailizer
+  
+  eg.
+  var a;
+
+  var keyword can't change the data type if initialized for first time but can change the value later
+
+
+   */
+}
