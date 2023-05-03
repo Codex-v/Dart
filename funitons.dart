66e2a130ff87d22d1b9b6398dac0e4d0b2b85d2d
VEDS03 <veds2103@gmail.com>
2023-05-04 00:51:44 +0530

    funtions in dart'

---
+++ funitons.dart
@@ -0,0 +1,16 @@
+/* type of funtions in dart 
+with arugments with return type
+no arugments no return type
+no arugments with return type
+with arugments no return type
+
+*/
+
+int ved(int a, int b) {
+  return a + b;
+}
+
+/* funtions or methods are a set of codes */
+void main(List<String> args) {
+  print(ved(5, 6));
+}
