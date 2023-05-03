995c6cbaa344920833b535ee73be74f6837e8a36
VEDS03 <veds2103@gmail.com>
2023-05-03 18:37:27 +0530

    data types in dart ;

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+int main() //main funtion
+{
+  //datatypes in dart
+  //int
+  int l = 45;
+  double d = 75.8;
+  String n = "ved";
+  bool t = true;
+  List keys = ['v', 'e', 'd']; //list is like a arrays
+  Map ved = new Map();
+  ved['surname'] = "shah";
+  ved['brach'] = "computer science"; //map is like a  dictionarys and objects
+  print(ved);
+
+  return 0;
+}
