e3d879e781c12745ad162b2dcb8f54b1e7ec82b0
VEDS03 <veds2103@gmail.com>
2023-05-05 00:41:30 +0530

    map in dart

---
+++ map.dart
@@ -0,0 +1,7 @@
+/* maps in dart are nothing but the objects and dictionary which has key paris values */
+void main(List<String> args) {
+  Map m = Map<int, String>();
+  m[77] = "ved";
+  m[7] = 'shah';
+  print(m);
+}
