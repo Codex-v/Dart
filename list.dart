56a03d1f3c6bb4e7446b954f1aceb3ad47b7fb34
VEDS03 <veds2103@gmail.com>
2023-05-05 00:41:06 +0530

    lists in dart

---
+++ list.dart
@@ -0,0 +1,22 @@
+/* list datatype in dart */
+
+void main(List<String> args) {
+/* in Dart, arrays are list objects and elements are ordered in sequence in a LIST 
+there are two type of list in dart
+
+1- fixed length list
+2- growable list
+
+
+
+*/
+//fixed list
+
+  List<int> list = List<int>.filled(5, 0);
+  // print(list);
+
+  //growable list
+
+  List<int> listG = [5, 7, 8, 9, 1, 3];
+  print(listG);
+}
